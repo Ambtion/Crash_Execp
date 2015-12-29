3b401385ff0321bbfc5bec52ee5b59c61fc0f052
LingJunHou <kequ1988@gmail.com>
2015-12-29 15:13:22 +0800

    Init Data

---
+++ Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by quke on 15/12/29.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by quke on 15/12/29.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
